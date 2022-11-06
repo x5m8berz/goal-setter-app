734e51c71251c69958b952efafc9604ae5fc3368
Hanrocky <hanrocky.halim@gmail.com>
2022-11-07 00:03:11 +0700

    second commit
    Add get request

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,11 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get('/api/goals',(req,res) => {
+    res.json({message:'get goals'})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
 
