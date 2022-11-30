42fc11c607bff73470fcb2bd4452166c518f66cd
Hanrocky <hanrocky.halim@gmail.com>
2022-11-30 16:07:33 +0700

    Added login logout feature

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,48 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice'
+import authService from '../features/auth/authService'
 
 function Header() {
-  return (
-    <header className='header'>
-        <div className='logo'>
-            <Link to='/'>Goal Setter</Link>
-        </div>
-        <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt/> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUser/> Register
-                </Link>
-            </li>
-        </ul>
-    </header>
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/') 
+    }
+
+
+    return (
+        <header className='header'>
+            <div className='logo'>
+                <Link to='/'>Goal Setter</Link>
+            </div>
+            <ul>
+                { user ? ( 
+                    <li>
+                    <button className='btn' onClick={onLogout}>
+                        <FaSignOutAlt/> Logout
+                    </button>
+                    </li>
+                ) : (
+                <>
+                    <li>
+                    <Link to='/login'>
+                        <FaSignInAlt/> Login
+                    </Link>
+                    </li>
+                    <li>
+                        <Link to='/register'>
+                            <FaUser/> Register
+                        </Link>
+                    </li>
+                </>) }
+                
+            </ul>
+        </header>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+import { useSelector , useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 
 
 function Login() {
@@ -9,6 +15,27 @@ function Login() {
   })
 
   const { email, password} = formData
+  
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector(
+    (state) => state.auth ) 
+
+  useEffect(() => {
+      if(isError){
+        toast.error(message)
+      }
+
+      if(isSuccess || user){
+        navigate('/')
+      }
+
+      dispatch(reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -20,6 +47,17 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
+  }
+
+  if(isLoading){
+    return <Spinner/>
   }
 
   return ( 
